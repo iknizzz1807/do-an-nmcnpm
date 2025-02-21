62b1a0a030cce62c4916bf667d7d37cac2c95ac2
kohrabi <63339885+kohrabi@users.noreply.github.com>
2025-02-21 21:45:15 +0700

    Initial Database implementation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,46 @@ export interface User {
   id: number;
   username: string;
 }
+
+export interface DoiBong {
+  maDoi: string;
+  tenDoi: string;
+  sanNha: string;
+}
+
+export interface CauThu {
+  maCT: string;
+  tenCT: string;
+  ngaySinh: Date;
+  loaiCT: number;
+  ghiChu: string;
+}
+
+export interface DSMuaGiai {
+  maMG: number;
+  tenMG: string;
+}
+
+export interface ThamGiaDB {
+  maDoi: string;
+  maCT: string;
+  maMG: number;
+}
+
+export interface LichThiDau {
+  maTD: string;
+  doiMot: string;
+  doiHai: string;
+  ngayGio: Date;
+  vongThiDau: number;
+  maMG: number;
+  doiThang: string;
+}
+
+export interface BanThang {
+  maTD: string;
+  maCT: string;
+  thoiDiem: number;
+  maDoi: string;
+  loaiBanThang: string;
+}
