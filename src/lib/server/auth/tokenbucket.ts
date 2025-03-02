728f0cd81ccb23d3beeeb3fe63b01d8a06d7b19c
kohrabi <63339885+kohrabi@users.noreply.github.com>
2025-03-02 21:41:34 +0700

    add: LuciaAuth TokenBucket and throttler

---
+++ src/lib/server/auth/tokenbucket.ts
@@ -0,0 +1,54 @@
+// Foreach key(aka User) how many request can it make before getting rejected
+// This will refill every refillIntervalSeconds
+export class TokenBucketRateLimit<_Key> {
+	public max: number;
+	public refillIntervalSeconds: number;
+
+	constructor(max: number, refillIntervalSeconds: number) {
+		this.max = max;
+		this.refillIntervalSeconds = refillIntervalSeconds;
+	}
+
+	private storage = new Map<_Key, Bucket>();
+
+	public check(key: _Key, cost: number): boolean {
+		const bucket = this.storage.get(key) ?? null;
+		if (bucket === null) {
+			return true;
+		}
+		const now = Date.now();
+		const refill = Math.floor((now - bucket.refilledAt) / (this.refillIntervalSeconds * 1000));
+		if (refill > 0) {
+			return Math.min(bucket.count + refill, this.max) >= cost;
+		}
+		return bucket.count >= cost;
+	}
+
+	public consume(key: _Key, cost: number): boolean {
+		let bucket = this.storage.get(key) ?? null;
+		const now = Date.now();
+		if (bucket === null) {
+			bucket = {
+				count: this.max - cost,
+				refilledAt: now
+			};
+			this.storage.set(key, bucket);
+			return true;
+		}
+		// Number of token will be added
+		const refill = Math.floor((now - bucket.refilledAt) / (this.refillIntervalSeconds * 1000));
+		bucket.count = Math.min(bucket.count + refill, this.max);
+		bucket.refilledAt = bucket.refilledAt + refill * this.refillIntervalSeconds * 1000;
+		if (bucket.count < cost) {
+			return false;
+		}
+		bucket.count -= cost;
+		this.storage.set(key, bucket);
+		return true;
+	}
+}
+
+interface Bucket {
+	count: number;
+	refilledAt: number;
+}
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,12 +1,27 @@
 import { verifyPasswordHash } from "$lib/server/auth/password";
+import { Throttler } from "$lib/server/auth/throttler";
+import { TokenBucketRateLimit } from "$lib/server/auth/tokenbucket";
 import { createSession, generateSessionToken, setSessionTokenCookie } from "$lib/server/db/functions/Session";
 import { selectUserFromEmail, selectUserPasswordHash, verifyEmailInput } from "$lib/server/db/functions/User";
 import { redirect } from "@sveltejs/kit";
 import { fail, type Actions } from "@sveltejs/kit"
 
+// Limit Retry request
+const throttler = new Throttler<number>([0, 1, 2, 4, 8, 16, 30, 60, 180, 300]);
+// Ratelimit
+const ipBucket = new TokenBucketRateLimit<string>(20, 1);
+
 export const actions : Actions =  {
   default: async (event) => {
-    // const clientIP = event.request.headers.get("X-Forwarded-For");
+    // TODO: Assumes X-Forwarded-For is always included.
+    const clientIP = event.getClientAddress();
+    if (clientIP !== null && !ipBucket.check(clientIP, 1)) {
+      return fail(429, {
+        message: "Too many requests",
+        email: ""
+      });
+    }
+
     const formData = await event.request.formData();
     const email = formData.get("email");
     const password = formData.get("password");
@@ -36,6 +51,19 @@ export const actions : Actions =  {
         email
       });
     }
+    if (clientIP !== null && !ipBucket.consume(clientIP, 1)) {
+      return fail(429, {
+        message: "Too many requests",
+        email: ""
+      })
+    }
+    if (!throttler.consume(user?.id)) {
+      return fail(429, {
+        message: "Too many requests",
+        email: ""
+      })
+    }
+
     const passwordHash = await selectUserPasswordHash(user.id);
     const validPassword = await verifyPasswordHash(passwordHash, password);
     if (!validPassword) {
@@ -44,6 +72,7 @@ export const actions : Actions =  {
         email
       });
     }
+	  throttler.reset(user.id);
     const sessionToken = generateSessionToken();
     const session = await createSession(sessionToken, user.id);
     setSessionTokenCookie(event, sessionToken, session.expiresAt);
