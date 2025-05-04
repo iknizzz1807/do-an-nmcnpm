e93c712b4c6b0b057180199041e9757daa6e44e7
kohrabi <63339885+kohrabi@users.noreply.github.com>
2025-05-04 10:00:23 +0700

    Add: Sắp xếp lịch tự động

--- src/lib/server/db/functions/LichThiDau.ts
+++ src/lib/server/db/functions/LichThiDau.ts
@@ -1,4 +1,4 @@
-import { eq, and, getTableColumns } from 'drizzle-orm';
+import { eq, and, getTableColumns, inArray } from 'drizzle-orm';
 import { db } from '../client';
 import { LichThiDauTable } from '../schema/LichThiDau';
 import type { LichThiDau } from '$lib/typesDatabase';
@@ -7,6 +7,8 @@ import { MuaGiaiTable } from '../schema/MuaGiai';
 import { alias } from 'drizzle-orm/sqlite-core';
 import { BanThangTable } from '../schema/BanThang';
 import { ThePhatTable } from '../schema/ThePhat';
+import { choose, randIntBetween } from '$lib/server/utils';
+import { TrongTaiTable } from '../schema/TrongTai';
 
 export const insertLichThiDau = async (...lichThiDau: LichThiDau[]) => {
     let returning = await db.insert(LichThiDauTable).values(lichThiDau).returning({ id: LichThiDauTable.maTD });
@@ -50,6 +52,64 @@ export const selectLichThiDauMaTD = async(maTD: number) => {
     .from(LichThiDauTable)
     .where(eq(LichThiDauTable.maTD, maTD)).limit(1))
     .at(0) satisfies LichThiDau | undefined;
+}
+
+export const tuDongSapXep = async (maMG: number, maDBs : number[]) => {
+  let ids : number[] = [];
+  const doiBongs = await db.select().from(DoiBongTable).where(inArray(DoiBongTable.maDoi, maDBs));
+  const trongTais = await db.select().from(TrongTaiTable);
+  if (trongTais.length == 0)
+    throw new Error("Không có trọng tài nào hết");
+  
+  while(doiBongs.length >= 2) {
+      const doiMotIndex = randIntBetween(0, doiBongs.length - 1);
+      const doiHaiIndex = randIntBetween(0, doiBongs.length - 1);
+      if (doiMotIndex == doiHaiIndex)
+        continue;
+      const lichThiDau : LichThiDau = {
+        maMG: maMG,
+        maVTD: randIntBetween(1, 2),
+        maSan: doiBongs[doiMotIndex].maSan,
+
+        doiHai: doiBongs[doiHaiIndex].maDoi,
+        doiMot: doiBongs[doiMotIndex].maDoi,
+        doiThang: null,
+        
+        ngayGioDuKien: new Date().toJSON(),
+        ngayGioThucTe: new Date().toJSON(),
+        
+        thoiGianDaThiDau: 0,
+        maTT: trongTais[randIntBetween(0, trongTais.length - 1)].maTT
+      }
+      
+      // const lichThiDau2 : LichThiDau = {
+      //   maVTD: randIntBetween(1, 2),
+      //   maMG: maMG,
+      //   maSan: doiBongs[doiHaiIndex].maSan,
+
+      //   doiMot: doiBongs[doiHaiIndex].maDoi,
+      //   doiHai: doiBongs[doiMotIndex].maDoi,
+      //   doiThang: null,
+        
+      //   ngayGioDuKien: new Date().toJSON(),
+      //   ngayGioThucTe: new Date().toJSON(),
+        
+      //   thoiGianDaThiDau: 0,
+      //   maTT: trongTais[randIntBetween(0, trongTais.length - 1)].maTT
+      // }
+      doiBongs.splice(doiMotIndex, 1);
+      // doiBongs.splice(doiHaiIndex, 1);
+
+      await insertLichThiDau(lichThiDau).then(
+          (value) => ids.push(...value.map((val) => val.id)),
+          (err) => { if (err) throw err; }
+      ); 
+      // await insertLichThiDau(lichThiDau2).then(
+      //     (value) => ids.push(...value.map((val) => val.id)),
+      //     (err) => { if (err) throw err; }
+      // );
+  }
+  return ids;
 }
 
 export const selectAllLichThiDauWithName = async(maMG: number) => {
