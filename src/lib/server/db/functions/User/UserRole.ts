0d0d97633a04ce2d08acb2cd80294a3d8aa7d1b3
kohrabi <63339885+kohrabi@users.noreply.github.com>
2025-05-05 23:11:10 +0700

    fix editable

--- src/lib/server/db/functions/User/UserRole.ts
+++ src/lib/server/db/functions/User/UserRole.ts
@@ -27,12 +27,12 @@ export const checkPageViewable = async (groupId : number, pageName: string) => {
 }
 
 export const checkPageEditable = async (groupId : number, pageName: string) => {
-  const role = (await db.select(getTableColumns(UserRoleTable)).from(HasRoleTable)
-    .innerJoin(UserRoleTable, eq(UserRoleTable.roleId, HasRoleTable.roleId))
-    .where(and(eq(HasRoleTable.groupId, groupId), like(UserRoleTable.viewablePage, pageName)))
-    .groupBy(UserRoleTable.roleId)
-    .limit(1)).at(0) ?? null;
-  if (role === null)
-    return false;
-  return role.canEdit;
+  const roles = (await db.select(getTableColumns(UserRoleTable)).from(HasRoleTable)
+  .innerJoin(UserRoleTable, eq(UserRoleTable.roleId, HasRoleTable.roleId))
+  .where(eq(HasRoleTable.groupId, groupId))
+  .groupBy(UserRoleTable.roleId));
+  for (const role of roles)
+    if (pageName.match(role.viewablePage))
+      return role.canEdit;
+  return false;
 }
